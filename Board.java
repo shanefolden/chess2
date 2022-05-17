10a7b4ca8f09c7cafc0d4e6b2e649dac87a2d3de
Shane Folden <folden@google.com>
2022-05-17 00:36:17 -0700

    Created board object to draw chessboard

---
+++ Board.java
@@ -0,0 +1,54 @@
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public class Board extends JPanel
+{
+  BufferedImage buffered;
+  final int ROW_WIDTH = 805;
+  final int COL_HEIGHT = 805;
+  final int SQUARE_LENGTH = 115;
+
+  public Board(Graphics g){
+      drawBoard(g);
+  } 
+  
+  public void drawBoard(Graphics g){
+    drawLines(g);
+    drawSpaces(g);
+  }
+
+  
+
+  public void drawLines(Graphics g) {
+      
+    for (int i = 0; i < 8; i++) {
+      int height = i == 0 || i == 7 ? 15 : 10;
+      g.fillRect(100, i * 113 + 40, ROW_WIDTH, height);
+    }
+    // draw cols
+    for (int i = 0; i < 8; i++) {
+      int width = i == 0 || i == 7 ? 15 : 10;
+      g.fillRect(i * 113 + 100, 40, width, COL_HEIGHT);
+    }
+  }
+
+  public void drawSpaces(Graphics g){
+    for(int i = 0; i<7; i++){
+      for(int j = 0; j<7; j++){
+        Color squareColor = (i+j)%2 ==0 ? new Color(50,50,50) : new Color(200,200,200); 
+        g.setColor(squareColor); 
+        int xOffset = i == 0 || i == 7 ? 15 : 10;
+        int yOffset = j == 0 || j == 7 ? 15 : 10;
+
+        
+        g.fillRect(i*113 +100 + xOffset, j*113+40+yOffset, SQUARE_LENGTH - xOffset, SQUARE_LENGTH -yOffset);
+      }  
+    }
+  }
+}   
---
+++ Screen.java
@@ -0,0 +1,37 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.imageio.ImageIO;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Screen extends JPanel {
+
+
+  public Dimension getPreferredSize() {
+    //Sets the size of the panel
+    return new Dimension(1200, 1000);
+  }
+
+
+  public void paintComponent(Graphics g) {
+    super.paintComponent(g);
+
+    Board board = new Board(g);
+    
+
+  }
+
+ 
+}
